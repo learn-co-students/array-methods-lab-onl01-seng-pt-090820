de4ee70f46ed1fefcb6330771df2d2ff1dfec44b
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-16 03:22:06 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,4 +1,4 @@
-def using_include (array.arg, element.arg)
+def using_include (array, element)
   # array = ["wow", "I", "am", "really", "learning", "arrays!"]
   # element1.include? ("wow")
   # element2.include? ("Tardis")
